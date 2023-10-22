a2e260cea6cb09544891d059414b5dec46ea5d64
John Cruz <johnamcruz@gmail.com>
2023-10-21 16:01:58 -1000

    reverrt

--- Source/StockProphet/Services/CompanyService.swift
+++ Source/StockProphet/Services/CompanyService.swift
@@ -20,26 +20,8 @@ class CompanyService: CompanyServiceable {
     func load(query: String) async -> [Company] {
         var results: [Company] = []
         do {
-            return try await withThrowingTaskGroup(of: Company.self) { group in
-                let response = try await client.getTicker(ticker: "", query: query, order: .asc)
-                results = response.results.map{ $0.toCompany() }
-                
-                for company in results {
-                    group.addTask{
-                        let company = company
-                        async let details = self.client.getTickerDetails(ticker: company.ticker)
-                        async let price = self.client.getDailyOpenClose(ticker: company.ticker, date: Date())
-                        return try await Company(name: details.results.name, ticker: company.ticker, price: price.close)
-                    }
-                }
-                
-                var results: [Company] = []
-                for try await company in group {
-                    results.append(company)
-                }
-                return results
-            }
-            
+            let response = try await client.getTicker(ticker: "", query: query, order: .asc)
+            results = response.results.map{ $0.toCompany() }
         } catch {
             debugPrint(error)
         }
