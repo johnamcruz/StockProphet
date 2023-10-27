d47a0b5e6bc584a3b830eaebd73af0121f4086dd
John Cruz <johnamcruz@gmail.com>
2023-10-26 16:47:04 -1000

    updated

--- Source/StockProphet/Services/StockForecastingService.swift
+++ Source/StockProphet/Services/StockForecastingService.swift
@@ -6,8 +6,32 @@
 //
 
 import Foundation
+import CoreML
+
+enum StockForecastingServiceError: Error {
+    case modelLoadingError
+}
 
 protocol StockForecastingServiceable {
-    func predict(original: Stock) async throws -> Stock
     func predict(original: [Stock]) async throws -> [Stock]
+}
+
+class StockForecastingService: StockForecastingServiceable {
+    var model: StockForecaster?
+    
+    init() {
+        let config = MLModelConfiguration()
+        self.model = try? StockForecaster(configuration: config)
+    }
+    
+    func predict(original: [Stock]) async throws -> [Stock] {
+        guard let model = self.model else {
+            throw StockForecastingServiceError.modelLoadingError
+        }
+        
+        let input = StockForecasterInput(lstm_input: MLMultiArray())
+        let output = try await model.prediction(input: input)
+        
+        return []
+    }
 }
